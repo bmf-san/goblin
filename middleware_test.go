5ab461e12b8d2a93bbca91ffe5d0dd456d834715
bmf-san <bmf.infomation@gmail.com>
2022-07-02 21:52:53 +0900

    [update] add benchmark tests

--- middleware_test.go
+++ middleware_test.go
@@ -16,12 +16,6 @@ func TestNewMiddleware(t *testing.T) {
 	}
 }
 
-func handler(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(w, r)
-	})
-}
-
 func TestThen(t *testing.T) {
 	t.Skip("This method covers the tests in an associative manner with router tests, so skip it.")
 }
