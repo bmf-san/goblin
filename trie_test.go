453971ca812613dc77500b5c5e2f2daa6dce20ad
bmf-san <bmf.infomation@gmail.com>
2023-01-11 00:08:16 +0900

    [update] refactoring

--- trie_test.go
+++ trie_test.go
@@ -21,6 +21,33 @@ func TestNewTree(t *testing.T) {
 	}
 }
 
+func TestGetParamsAndPutParams(t *testing.T) {
+	params := &[]Param{
+		{
+			key:   "id",
+			value: "123",
+		},
+		{
+			key:   "name",
+			value: "john",
+		},
+	}
+
+	tree := newTree()
+	tree.paramsPool.New = func() interface{} {
+		// NOTE: It is better to set the maximum value of paramters to capacity.
+		return &[]Param{}
+	}
+	params = tree.getParams()
+	tree.putParams(params)
+
+	expectedParams := &[]Param{}
+	actualParams := tree.getParams()
+	if !reflect.DeepEqual(actualParams, expectedParams) {
+		t.Errorf("actual:%v expected:%v", actualParams, expectedParams)
+	}
+}
+
 // item is a set of routing definition.
 type item struct {
 	method string
